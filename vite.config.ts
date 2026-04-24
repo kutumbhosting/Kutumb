e76c2fe3dc8b1bf5976d4558be7ee62eefbb7ee6
kutumbhosting <kutumbhosting@gmail.com>
2026-04-25 08:23:10 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === "development",
   ].filter(Boolean),
 
   resolve: {
