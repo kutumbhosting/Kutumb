e8c7252c3f00ba9de0c6389179d2a9f46af1049f
kutumbhosting <kutumbhosting@gmail.com>
2026-04-24 11:10:41 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -208,8 +208,8 @@ app.post("/api/members", (req, res) => {
    🚀 START SERVER
 ------------------------------*/
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on", PORT);
 });
