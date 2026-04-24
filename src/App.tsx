91f20df2d931f495cd9e6acfe8e43bdee06efb2e
kutumbhosting <kutumbhosting@gmail.com>
2026-04-24 13:12:58 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Executive from "./pages/Executive";
@@ -11,6 +12,7 @@ import Events from "./pages/Events";
 import Membership from "./pages/Membership";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import Admin from "./pages/Admin"; // 👈 ADD THIS
 
 const queryClient = new QueryClient();
 
@@ -19,19 +21,22 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/executive" element={<Executive />} />
-          <Route path="/activities" element={<Activities />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/membership" element={<Membership />} />
-          <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+
+      {/* ❌ REMOVE BrowserRouter FROM HERE */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/executive" element={<Executive />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/membership" element={<Membership />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* ✅ ADMIN ROUTE */}
+        <Route path="/admin" element={<Admin />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
