6e70dc2f82f95b92c4077851e9abf054227b1bee
kutumbhosting <kutumbhosting@gmail.com>
2026-04-26 22:44:29 +1000

    Update Events.tsx

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -62,7 +62,7 @@ useEffect(() => {
 }, []);
 
 const fetchUpcomingEvents = async () => {
-  const res = await fetch("http://localhost:5000/api/upcoming-events");
+  const res = await fetch("api/upcoming-events");
   const data = await res.json();
   setUpcomingEvents(data);
 };
@@ -90,7 +90,7 @@ const fetchUpcomingEvents = async () => {
   }
 
   try {
-    const res = await fetch("http://localhost:5000/api/events", {
+    const res = await fetch("api/events", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify(formData),
